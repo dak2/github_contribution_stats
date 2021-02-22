c61053d787e9907cbb3bf0e66587684939117c26
dak2 <dawt2h@gmail.com>
2021-02-23 00:43:01 +0900

    card baseコンポーネントのスタイル追加

--- src/components/atoms/CardBase.tsx
+++ src/components/atoms/CardBase.tsx
@@ -1,7 +1,12 @@
+import '../../styles/CardBase.css'
 
 function CardBase() {
   return (
-    <div>
+    <div className="l-wrapper_06">
+      <div className="card_06">
+        <div className="card-content_06">
+        </div>
+      </div>
     </div>
   );
 }
