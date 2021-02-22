2d463edea9557b8f071d2a7a5156737ba9feed9f
dak2 <dawt2h@gmail.com>
2021-02-23 01:27:44 +0900

    card baseコンポーネントに各コンポーネントを統合する

--- src/components/atoms/CardBase.tsx
+++ src/components/atoms/CardBase.tsx
@@ -1,11 +1,19 @@
+import BarChart from './BarChart';
+import PieChart from './PieChart';
+import UserIcon from './UserIcon';
 import '../../styles/CardBase.css'
 
 function CardBase() {
   return (
     <div className="l-wrapper_06">
-      <div className="card_06">
-        <div className="card-content_06">
-        </div>
+      <div className="user_icon">
+        <UserIcon />
+      </div>
+      <div className="piechart">
+        <PieChart />
+      </div>
+      <div className="histogramchart">
+        <BarChart />
       </div>
     </div>
   );
--- src/components/organisms/ContributtionCard.tsx
+++ src/components/organisms/ContributtionCard.tsx
@@ -1,7 +1,9 @@
+import CardBase from '../atoms/CardBase';
 
 function ContributtionCard() {
   return (
     <div>
+      <CardBase />
     </div>
   );
 }
