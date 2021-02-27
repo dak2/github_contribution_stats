aa384fa2eb9d8efed212c2ff062d71c9f6edba45
dak2 <dawt2h@gmail.com>
2021-02-28 00:15:32 +0900

    settings for module import alphabetized

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,20 +9,31 @@ module.exports = {
   },
   plugins: [
     '@typescript-eslint',
+    'import'
   ],
   extends: [
     'eslint:recommended',
-    'plugin:@typescript-eslint/eslint-recommended',
     'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/eslint-recommended',
     'plugin:react/recommended',
+    'plugin:import/errors',
     'prettier'
   ],
   settings: {
     react: {
       version: 'detect',
+    },
+    'import/resolver': {
+      node: {
+        extensions: ['.js', '.jsx', '.ts', '.tsx']
+      }
     }
   },
   env: {
-    "node": true
+    'node': true
+  },
+  rules: {
+    'sort-imports': 0,
+    'import/order': [2, { 'alphabetize': { 'order': 'asc' } }]
   }
 };
