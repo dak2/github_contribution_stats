b2bc350f8a7988cf871f98f816bd36ff2eef02f2
dak2 <dawt2h@gmail.com>
2021-02-28 00:16:07 +0900

    lint適用

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,12 @@
 import React from 'react';
 import './App.css';
 
-function App() {
+const App: React.FC = () => {
   return (
     <div className="App">
-      <header className="App-header">
-      </header>
+      <header className="App-header"></header>
     </div>
   );
-}
+};
 
 export default App;
