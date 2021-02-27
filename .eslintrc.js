459a893ad5e5fc141c797f9c0ce3882a8c5c14d1
dak2 <dawt2h@gmail.com>
2021-02-27 23:46:19 +0900

    settings eslintrc

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,12 +1,28 @@
 module.exports = {
   root: true,
   parser: '@typescript-eslint/parser',
+  parserOptions: {
+    sourceType: 'module',
+    ecmaVersion: 2020,
+    tsconfigRootDir: __dirname,
+    project: ['./tsconfig.eslint.json']
+  },
   plugins: [
     '@typescript-eslint',
   ],
   extends: [
-    'airbnb-base',
     'eslint:recommended',
+    'plugin:@typescript-eslint/eslint-recommended',
     'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended',
+    'prettier'
   ],
+  settings: {
+    react: {
+      version: 'detect',
+    }
+  },
+  env: {
+    "node": true
+  }
 };
