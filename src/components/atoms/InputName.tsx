2bc55933e000486d8f3572541197d78a608e0b13
dak2 <dawt2h@gmail.com>
2021-03-08 01:17:33 +0900

    form入力後にcardコンポーネントにリダイレクトする

--- src/components/atoms/InputName.tsx
+++ src/components/atoms/InputName.tsx
@@ -1,10 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useHistory } from 'react-router-dom';
+import { getGithubUser } from '../../functions/fetchGithubData';
 
 const InputName: React.FC = () => {
+  const history = useHistory();
+  const [data, setData] = useState({ name: '', githubData: {} });
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>): void => {
+    setData({ name: event.target.value, githubData: {} });
+  };
+
+  const getGithubData = (val: string) => {
+    console.log('test');
+    getGithubUser(val);
+    localStorage.setItem('githubUserName', val);
+    setTimeout(() => history.push('/card'), 1000);
+  };
+
   return (
     <form>
-      <input placeholder={'GithubUserName'} style={{ outline: 'none' }}></input>
-      <button type='submit'>Submit</button>
+      <input
+        placeholder={'GithubUserName'}
+        style={{ outline: 'none' }}
+        onChange={(event) => handleChange(event)}
+      ></input>
+      <button type="submit" onClick={() => getGithubData(data.name)}>
+        Submit
+      </button>
     </form>
   );
 };
