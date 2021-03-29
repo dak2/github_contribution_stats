f6c337399b9097720339ddf33bcf9c52f55a398f
dak2 <dawt2h@gmail.com>
2021-03-29 15:36:21 +0900

    locationからparamsを取得してCardBaseコンポーネントに渡す

--- src/components/organisms/ContributtionCard.tsx
+++ src/components/organisms/ContributtionCard.tsx
@@ -2,12 +2,19 @@ import React from 'react';
 import { useLocation } from 'react-router-dom';
 import CardBase from '../molecules/CardBase';
 
+type LocationState = {
+  user: any;
+  commits: any;
+};
+
 const ContributtionCard: React.FC = () => {
-  const location = useLocation();
+  const location = useLocation<LocationState>();
+  const { user, commits } = location.state;
   console.log('location', location);
+
   return (
     <div>
-      <CardBase />
+      <CardBase user={user} commits={commits} />
     </div>
   );
 };
