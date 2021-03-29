2e95d384a03e98de947b94f13a79a6945bc73c6a
dak2 <dawt2h@gmail.com>
2021-03-29 15:37:16 +0900

    propsを各コンポーネントに渡す - UserIconコンポーネント - BarChartコンポーネント

--- src/components/molecules/CardBase.tsx
+++ src/components/molecules/CardBase.tsx
@@ -4,17 +4,44 @@ import BarChart from '../atoms/BarChart';
 import PieChart from '../atoms/PieChart';
 import UserIcon from '../atoms/UserIcon';
 
-const CardBase: React.FC = () => {
+type Commits = {
+  id: string;
+  type: string;
+  actor: Record<string, unknown>;
+  repo: Record<string, unknown>;
+  payload: Record<string, unknown>;
+};
+
+type User = {
+  avatar_url: string;
+  login: string;
+  url: string;
+};
+
+type Props = {
+  user: User;
+  commits: Commits[];
+};
+
+const CardBase: React.FC<Props> = (props: Props) => {
+  const { user, commits } = props;
+  const pushEvents = commits.filter((commit) => {
+    return commit.type == 'PushEvent';
+  });
   return (
     <div className={style.l_wrapper_06}>
       <div className={style.user_icon}>
-        <UserIcon />
+        <UserIcon
+          avatarUrl={user.avatar_url}
+          name={user.login}
+          url={user.url}
+        />
       </div>
       <div className={style.pie_chart}>
         <PieChart />
       </div>
       <div className={style.bar_chart}>
-        <BarChart />
+        <BarChart commits={pushEvents}/>
       </div>
     </div>
   );
