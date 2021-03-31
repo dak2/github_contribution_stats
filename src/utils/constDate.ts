59df3573be001843c7fa89896396917ca8e87567
dak2 <dawt2h@gmail.com>
2021-04-01 00:12:30 +0900

    日時比較用に1週間分のDateを用意する

--- src/utils/constDate.ts
+++ src/utils/constDate.ts
@@ -1,17 +1,47 @@
-import { add } from 'date-fns';
+import { add, startOfDay, endOfDay } from 'date-fns';
 import { format, utcToZonedTime } from 'date-fns-tz';
+
+type weekDays = {
+  index: number;
+  dates: {
+    start: string;
+    end: string;
+  };
+};
 
 export const timeZone = 'Asia/Tokyo';
 export const today = new Date();
-export const weekDays = (today: Date): string[] => {
+export const weekDays = (today: Date): weekDays[] => {
   return [...Array(7)].map((_, index) => {
+    // indexが0の場合は当日を含めるようにしている
     if (index == 0) {
-      return format(utcToZonedTime(today, timeZone), 'yyyy-MM-dd HH:mm:ss zzz');
+      return {
+        index: index + 1,
+        dates: {
+          start: format(
+            utcToZonedTime(startOfDay(today), timeZone),
+            'yyyy-MM-dd HH:mm:ss',
+          ),
+          end: format(
+            utcToZonedTime(endOfDay(today), timeZone),
+            'yyyy-MM-dd HH:mm:ss',
+          ),
+        },
+      };
     } else {
-      return format(
-        utcToZonedTime(add(today, { days: -index }), timeZone),
-        'yyyy-MM-dd HH:mm:ss zzz',
-      );
+      return {
+        index: index + 1,
+        dates: {
+          start: format(
+            utcToZonedTime(startOfDay(add(today, { days: -index })), timeZone),
+            'yyyy-MM-dd HH:mm:ss',
+          ),
+          end: format(
+            utcToZonedTime(endOfDay(add(today, { days: -index })), timeZone),
+            'yyyy-MM-dd HH:mm:ss',
+          ),
+        },
+      };
     }
   });
 };
