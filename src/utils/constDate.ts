46bd093266d927182be987ceec82bd2c73d7e45b
dak2 <dawt2h@gmail.com>
2021-04-02 00:46:10 +0900

    delete index in weekDays type

--- src/utils/constDate.ts
+++ src/utils/constDate.ts
@@ -2,10 +2,9 @@ import { add, startOfDay, endOfDay } from 'date-fns';
 import { format, utcToZonedTime } from 'date-fns-tz';
 
 type weekDays = {
-  index: number;
   dates: {
-    start: string;
-    end: string;
+    start: Date;
+    end: Date;
   };
 };
 
@@ -13,35 +12,23 @@ export const timeZone = 'Asia/Tokyo';
 export const today = new Date();
 export const weekDays = (today: Date): weekDays[] => {
   return [...Array(7)].map((_, index) => {
-    // indexが0の場合は当日を含めるようにしている
-    if (index == 0) {
-      return {
-        index: index + 1,
-        dates: {
-          start: format(
-            utcToZonedTime(startOfDay(today), timeZone),
-            'yyyy-MM-dd HH:mm:ss',
-          ),
-          end: format(
-            utcToZonedTime(endOfDay(today), timeZone),
-            'yyyy-MM-dd HH:mm:ss',
-          ),
-        },
-      };
-    } else {
-      return {
-        index: index + 1,
-        dates: {
-          start: format(
-            utcToZonedTime(startOfDay(add(today, { days: -index })), timeZone),
-            'yyyy-MM-dd HH:mm:ss',
-          ),
-          end: format(
-            utcToZonedTime(endOfDay(add(today, { days: -index })), timeZone),
-            'yyyy-MM-dd HH:mm:ss',
-          ),
-        },
-      };
-    }
+    const startDate = new Date(
+      format(
+        utcToZonedTime(startOfDay(add(today, { days: -index })), timeZone),
+        'yyyy-MM-dd HH:mm:ss',
+      ),
+    );
+    const endDate = new Date(
+      format(
+        utcToZonedTime(endOfDay(add(today, { days: -index })), timeZone),
+        'yyyy-MM-dd HH:mm:ss',
+      ),
+    );
+    return {
+      dates: {
+        start: startDate,
+        end: endDate,
+      },
+    };
   });
 };
