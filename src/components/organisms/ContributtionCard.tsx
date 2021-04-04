00996ffd4455f513b8af54e2bfcf641d3bfc359f
dak2 <dawt2h@gmail.com>
2021-04-04 16:50:37 +0900

    path直接入力した際にcardコンポーネントに遷移しない

--- src/components/organisms/ContributtionCard.tsx
+++ src/components/organisms/ContributtionCard.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { useLocation } from 'react-router-dom';
 import { Events, User } from '../../utils/propsType';
 import CardBase from '../molecules/CardBase';
+import InputForm from '../molecules/InputForm';
 
 type LocationState = {
   user: User;
@@ -10,13 +11,20 @@ type LocationState = {
 
 const ContributtionCard: React.FC = () => {
   const location = useLocation<LocationState>();
-  const { user, events } = location.state;
-
-  return (
-    <div>
-      <CardBase user={user} events={events} />
-    </div>
-  );
+  if (location.state != undefined) {
+    const { user, events } = location.state;
+    return (
+      <div>
+        <CardBase user={user} events={events} />
+      </div>
+    );
+  } else {
+    return (
+      <div>
+        <InputForm />
+      </div>
+    );
+  }
 };
 
 export default ContributtionCard;
